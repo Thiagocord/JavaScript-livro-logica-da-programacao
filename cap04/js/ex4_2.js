bac47c714981f5c93a9d7e97fa343937156051eb
Thiagocord <thiago_cordeiro_84@hotmail.com>
2025-10-06 22:19:36 -0300

    good

--- cap04/js/ex4_2.js
+++ cap04/js/ex4_2.js
@@ -1,21 +1,15 @@
-const frm = document.querySelector("form")
-const res = document.getElementById("resposta")
-
-
+const frm = document.querySelector("form");
+const res = document.getElementById("resposta");
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()
-
+    e.preventDefault();
     const nome = frm.innome.value.trim();
     const masculino = frm.inmasculino.checked;
     const altura = Number(frm.inaltura.value);
-
-
+    let pesoIdeal;
     if (masculino) {
-        pesoIdeal = 22 * (altura * altura);
+        pesoIdeal = 22 * Math.pow(altura, 2);
     } else {
-        pesoIdeal = 21 * (altura * altura);
+        pesoIdeal = 21 * Math.pow(altura, 2);
     }
-
-    res.innerText = `${nome} seu peso ideal é ${pesoIdeal.toFixed(2)} kg.`
-        
-})
+    res.innerText = `${nome} seu peso ideal é ${pesoIdeal.toFixed(3)} kg.`
+});
