cc750635565e18584845b22cf9677eb3a326bc8c
Thiagocord <thiago_cordeiro_84@hotmail.com>
2025-10-09 23:21:29 -0300

    up

--- cap04/nodejs/ex4_7.js
+++ cap04/nodejs/ex4_7.js
@@ -1,14 +1,14 @@
-const prompt = require("prompt-sync")
-const pessoas = Number(prompt("Nº Pessoas: "))
-const peixes = Number(prompt("Nº Peixes: "))
-let pagar
+const prompt = require("prompt-sync")();
+const pessoas = Number(prompt("Nº Pessoas: "));
+const peixes = Number(prompt("Nº Peixes: "));
+let pagar;
 if (peixes <= pessoas) {
     pagar = pessoas * 20
-}else {
-    pagar = (pessoas * 20) + ((peixes - pessoas) + 12)
+} else {
+    pagar = (pessoas * 20) + ((peixes - pessoas) * 12)
 }
 
-console.log(`Pagar R$: ${pagar.toFixed(2)}`)
+console.log(`Pagar R$: ${pagar.toFixed(2)}`);
 
 
 
