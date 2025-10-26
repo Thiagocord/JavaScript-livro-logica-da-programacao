b45c71cbaf1dcdd06bc66915128871ac48af02b2
Thiagocord <thiago_cordeiro_84@hotmail.com>
2025-10-26 17:15:54 -0300

    gg

--- cap05/js/ex5_6.js
+++ cap05/js/ex5_6.js
@@ -5,7 +5,7 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault();
     const num = Number(frm.innumero.value);
     
-    if (num % 2 === 0 && num !== 2) {
+    if ((num % 2 === 0 && num !== 2) || (num === 1)) {
         resp.innerText = `${num} não é um número Primo.`
     } else {
         resp.innerText = `${num} é um número Primo.`
