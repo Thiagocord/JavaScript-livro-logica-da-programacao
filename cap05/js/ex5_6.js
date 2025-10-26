f4b8cfc0d513c5cb423eb5d93bed62511cf91419
Thiagocord <thiago_cordeiro_84@hotmail.com>
2025-10-26 17:03:45 -0300

    up

--- cap05/js/ex5_6.js
+++ cap05/js/ex5_6.js
@@ -3,7 +3,13 @@ const resp = document.querySelector("h3");
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
-
-
+    const num = Number(frm.innumero.value);
+    
+    if (num % 2 === 0 && num !== 2) {
+        resp.innerText = `${num} não é um número Primo.`
+    } else {
+        resp.innerText = `${num} é um número Primo.`
+    }        
+        
 
 });
