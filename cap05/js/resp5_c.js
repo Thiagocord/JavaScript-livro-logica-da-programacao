e837d5fce92b7cadbe3cd5d3f91afe6e238b49f6
Thiagocord <thiago_cordeiro_84@hotmail.com>
2025-11-20 00:47:02 -0300

    treino

--- cap05/js/resp5_c.js
+++ cap05/js/resp5_c.js
@@ -3,18 +3,35 @@ const resp1 = document.getElementById("resp1");
 const resp2 = document.getElementById("resp2");
 
 
-frm.addEventListener("submit" , (e)=> {
+frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const num = Number(frm.inNumero.value);
-    
 
-    for(let i = 1; i < num ; i++){
+    let resposta = [];
+    let soma = 0;
+
+    for (let i = 1; i < num; i++) {
         if (num % i == 0) {
-            resposta += `${i}, `
+            resposta.push(i);
             soma += i
         }
     }
 
+    const perfeitoSN = (soma === num) ? `${num} é perfeito` : `${num} não é perfeito`;
+
+    resp1.innerText = `Divisores do ${num}: ${resposta.join(", ")} (soma: ${soma})\n${perfeitoSN}`;
+
+
+
+
 
 })
+
+let n = 7
+
+const numero = [1, 2, 3]
+
+numero[n] = "a"
+
+console.log(numero)
---
+++ cap06/js/ex06_1.js
@@ -0,0 +1,19 @@
+const usuario = {
+  nome: 'Ana',
+  assinante: false
+};
+
+const statusAssinatura = `${usuario.nome} ${usuario.assinante ? 'é' : 'não é'} assinante.`;
+
+console.log(statusAssinatura); // Saída: Ana é assinante.
+
+
+
+
+
+
+const meuArray = [1,2,3,4]
+
+const meuArray2 = [{
+    
+}]
