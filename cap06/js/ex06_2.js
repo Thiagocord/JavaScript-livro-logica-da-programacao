f8c254fc495e4bf8790553c4cf5f33fb074672a7
Thiagocord <thiago_cordeiro_84@hotmail.com>
2025-11-21 00:05:25 -0300

    up

---
+++ cap06/js/ex06_2.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form");
+const resp1 = document.getElementById("resp1");
+const resp2 = document.getElementById("resp2");
+const resp3 = document.getElementById("resp3");
+
+const numeroSorteio = [];
+const numerosApostados = [];
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const num = Number(frm.inNumero.value);
+
+
+})
